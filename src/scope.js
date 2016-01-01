a1cba60cc0c36430e6e3a3bb549a1c40581e27f9
Iurii Kyrylenko <iurii_kyrylenko@Iuriis-MacBook-Pro.local>
2016-01-01 21:09:09 +0200

    TC:compares based on value if enabled; TC:correctly handles NaNs

--- src/scope.js
+++ src/scope.js
@@ -7,12 +7,13 @@ function Scope() {
     this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
 
     var watcher = {
         watchFn: watchFn,
         listenerFn: listenerFn || function() {},
-        last: initWatchVal
+        last: initWatchVal,
+        valueEq: !!valueEq
     };
 
     this.$$watchers.push(watcher);
@@ -42,9 +43,9 @@ Scope.prototype.$$digestOnce = function() {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
 
-        if(newValue !== oldValue) {
+        if(!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
             self.$$lastDirtyWatch = watcher;
-            watcher.last = newValue;
+            watcher.last = watcher.valueEq ? _.cloneDeep(newValue) : newValue;
             watcher.listenerFn(
                 newValue,
                 oldValue === initWatchVal ? newValue : oldValue,
@@ -57,4 +58,14 @@ Scope.prototype.$$digestOnce = function() {
     });
 
     return dirty;
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+    if(valueEq) {
+        return _.isEqual(newValue, oldValue);
+    } else {
+        return (newValue === oldValue) ||
+            typeof newValue === 'number' && typeof oldValue === 'number' &&
+            isNaN(newValue) && isNaN(oldValue);
+    }
 };
